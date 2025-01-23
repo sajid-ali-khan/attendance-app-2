08eeea81857a142ff57e276a4841e2c321e1b4e5
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-01-23 19:42:42 +0530

    add functionality to semester dates fragment

--- app/src/main/java/com/example/attendance_app_2/db/DatesHelper.kt
+++ app/src/main/java/com/example/attendance_app_2/db/DatesHelper.kt
@@ -9,16 +9,18 @@ import kotlinx.coroutines.withContext
 object DatesHelper {
     val TAG = this::class.java.simpleName
     suspend fun fetchDates(): SemesterDates {
-        val query = "SELECT start_date, end_date FROM semester_dates"
+        val query = "select convert(varchar, start_date, 3) as start_date, convert(varchar, end_date, 3) as end_date from semester_dates"
         return withContext(Dispatchers.IO){
             var date = SemesterDates("", "")
             try {
                 DatabaseHelper.getConnection()?.use { connection ->
                     connection.createStatement().use { st ->
                         st.executeQuery(query).use {
-                            val startDate= it.getString("start_date")
-                            val endDate = it.getString("end_date")
-                            date = SemesterDates(startDate, endDate)
+                            if (it.next()) {
+                                val startDate= it.getString("start_date")
+                                val endDate = it.getString("end_date")
+                                date = SemesterDates(startDate, endDate)
+                            }
                         }
                     }
                 }
@@ -26,6 +28,30 @@ object DatesHelper {
                 Log.e(TAG, "fetchDates: SQl error", e)
             }
             date
+        }
+    }
+
+    suspend fun saveDates(startDate: String, endDate: String): Boolean{
+        val query = "UPDATE semester_dates SET start_date = CONVERT(DATE, ?, 3),\n" +
+                "    end_date = CONVERT(DATE, ?, 3)\n" +
+                "WHERE id = 1"
+        return withContext(Dispatchers.IO){
+            var datesSaved = false
+            try{
+                DatabaseHelper.getConnection()?.use{connection->
+                    connection.prepareStatement(query).use{pst ->
+                        pst.setString(1, startDate)
+                        pst.setString(2, endDate)
+                        val rowsAffected = pst.executeUpdate()
+                        if (rowsAffected == 1){
+                            datesSaved = true
+                        }
+                    }
+                }
+            }catch(e: Exception){
+                Log.e(TAG, "saveDates: SQL error", e)
+            }
+            datesSaved
         }
     }
 }
--- app/src/main/java/com/example/attendance_app_2/fragments/SemesterDatesFragment.kt
+++ app/src/main/java/com/example/attendance_app_2/fragments/SemesterDatesFragment.kt
@@ -1,17 +1,65 @@
 package com.example.attendance_app_2.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.attendance_app_2.R
 import com.example.attendance_app_2.databinding.FragmentSemesterDatesBinding
+import com.example.attendance_app_2.db.DatesHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+
 
 class SemesterDatesFragment : Fragment(R.layout.fragment_semester_dates) {
     private lateinit var bindings: FragmentSemesterDatesBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindings = FragmentSemesterDatesBinding.bind(view)
+
+        lifecycleScope.launch {
+            val dates = DatesHelper.fetchDates()
+            bindings.etStartDate.setText(dates.startDate)
+            bindings.etEndDate.setText(dates.endDate)
+        }
+
+        bindings.btnSave.setOnClickListener {
+            Toast.makeText(requireContext(), "Saving dates", Toast.LENGTH_SHORT).show()
+            val startDate = bindings.etStartDate.text.toString()
+            val endDate = bindings.etEndDate.text.toString()
+            if (validDate(startDate) && validDate(endDate)){
+                lifecycleScope.launch {
+                    val datesSaved = DatesHelper.saveDates(startDate, endDate)
+                    var verdict = "Couldn't save dates"
+                    if (datesSaved){
+                        verdict = "Dates successfully saved"
+                    }
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(requireContext(), verdict, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(requireContext(), "Invalid dates", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    fun validDate(dateString: String): Boolean {
+        val originalFormat = DateTimeFormatter.ofPattern("dd/MM/yy")
+        return try {
+            val sqlServerFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            val date = LocalDate.parse(dateString, originalFormat)
+
+            val sqlDate = date.format(sqlServerFormat)
+            true
+        } catch (e: DateTimeParseException) {
+            false
+        }
     }
 }
