2c40f757dc23884ef36b9b5c852588d61c00c142
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-01-25 08:57:35 +0530

    add update attendance card item ui

--- app/src/main/java/com/example/attendance_app_2/fragments/UpdateAttendanceFragment.kt
+++ app/src/main/java/com/example/attendance_app_2/fragments/UpdateAttendanceFragment.kt
@@ -1,8 +1,24 @@
 package com.example.attendance_app_2.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.attendance_app_2.R
+import com.example.attendance_app_2.databinding.FragmentUpdateAttendanceBinding
 
 class UpdateAttendanceFragment : Fragment(R.layout.fragment_update_attendance) {
+    private lateinit var bindings : FragmentUpdateAttendanceBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindings = FragmentUpdateAttendanceBinding.bind(view)
 
+        bindings.btnFetchSessions.setOnClickListener{
+            //get the entered date
+            //validate the date
+            //fetch assignments of that date
+            //handle the clicks
+            //fetch the students with the click
+            //display students in a recycler view
+        }
+    }
 }
