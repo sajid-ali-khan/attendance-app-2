2d9b05c899e44ff4704d00480f17396ab39a8c80
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-02-09 21:53:09 +0530

    display name and role in the navigation menu

--- app/src/main/java/com/example/attendance_app_2/DashboardActivity.kt
+++ app/src/main/java/com/example/attendance_app_2/DashboardActivity.kt
@@ -2,11 +2,13 @@ package com.example.attendance_app_2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.attendance_app_2.databinding.ActivityDashboardBinding
+import com.example.attendance_app_2.databinding.HeaderLayoutBinding
 import com.example.attendance_app_2.fragments.AssignClassFragment
 import com.example.attendance_app_2.fragments.HomeFragment
 import com.example.attendance_app_2.fragments.MarkAttendanceFragment
@@ -16,6 +18,7 @@ import com.example.attendance_app_2.fragments.UpdateAttendanceFragment
 import com.example.attendance_app_2.fragments.report_fragments.CustomAttendanceReportFragment
 import com.example.attendance_app_2.fragments.report_fragments.DefaultAttendanceReportFragment
 import com.example.attendance_app_2.sharedPrefs.SharedPrefs
+import com.google.android.material.navigation.NavigationView
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
@@ -35,6 +38,18 @@ class DashboardActivity : AppCompatActivity() {
         }
 
         val role = SharedPrefs.getRole(this)
+        val name = SharedPrefs.getName(this)
+
+        //displaying the name and the role of user in the nav header
+        val navView = findViewById<NavigationView>(R.id.navView)
+        val navBinding = HeaderLayoutBinding.bind(navView.getHeaderView(0))
+        navBinding.tvName.text = name
+        navBinding.tvRole.text = when (role){
+            "teacher" -> "Teacher"
+            "head" -> "Head Of Department"
+            "admin" -> "Admin"
+            else -> "Unknown"
+        }
 
         if (role == "teacher") {
             handleTeacherUI()
@@ -42,6 +57,9 @@ class DashboardActivity : AppCompatActivity() {
             handleHODUI()
         } else if (role == "admin") {
             handleAdminUI()
+        } else{
+            Toast.makeText(this, "Unknown role", Toast.LENGTH_SHORT).show();
+            goToLoginScreen()
         }
 
         toggle = ActionBarDrawerToggle(
